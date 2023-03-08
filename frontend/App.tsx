823c738c186eb1a2561711796c48764a842ea492
unknown <luizcvf@dcc.ufrj.br>
2023-03-07 21:10:11 -0300

    Fiz a Tela de Recuperar Senha

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import NewPasswordScreen from "./pages/NewPasswordScreen";
+import RecoverPasswordScreen from "./pages/RecoverPasswordScreen";
+import VerifyCodeScreen from "./pages/VerifyCodeScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NewPasswordScreen></NewPasswordScreen>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
