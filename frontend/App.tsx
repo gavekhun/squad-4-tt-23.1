fa9476b1a368c54803ca139753385de691ffc1a0
unknown <luizcvf@dcc.ufrj.br>
2023-03-13 19:23:51 -0300

    Fiz a tela de Meus Dados

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,9 +1,10 @@
 import ProductScreen from "./pages/ProductScreen";
 import AddProduct from './pages/AddProduct/AddProduct';
 import ProfileEdit from "./pages/ProfileEdit";
+import DataScreen from "./pages/DataScreen";
 
 export default function App() {
   return (
-    <ProfileEdit/>
+    <DataScreen/>
   );
 }
