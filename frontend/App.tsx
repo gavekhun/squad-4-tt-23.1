f4b0aa846614b6fbfb9990343080f0188676120d
unknown <luizcvf@dcc.ufrj.br>
2023-03-13 21:40:54 -0300

    Terminei a tela de Avaliações

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,9 +1,10 @@
 import ProductScreen from "./pages/ProductScreen";
 import AddProduct from './pages/AddProduct/AddProduct';
 import ProfileEdit from "./pages/ProfileEdit";
+import RateScreen from "./pages/RateScreen";
 
 export default function App() {
   return (
-    <ProfileEdit/>
+    <RateScreen/>
   );
 }
